52707a8749e61a053b51c733b4b3904a7f507cbf
Archie <yangyxcn@gmail.com>
2015-08-04 18:52:19 +0800

    Implement simple UI without dependency injection
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- app/src/main/java/me/codethink/dagger2demo/MainActivity.java
+++ app/src/main/java/me/codethink/dagger2demo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UserModel user = new UserModel();
+        ((TextView) findViewById(R.id.user_desc_line)).setText(user.id + "\n" + user.name + "\n" + user.gender);
     }
 
     @Override
---
+++ app/src/main/java/me/codethink/dagger2demo/UserModel.java
@@ -0,0 +1,16 @@
+package me.codethink.dagger2demo;
+
+/**
+ * Created by archie on 15/8/4.
+ */
+public class UserModel {
+    public final String id;
+    public final String name;
+    public final String gender;
+
+    public UserModel() {
+        id = "1";
+        name = "Archie Yang";
+        gender = "Male";
+    }
+}
